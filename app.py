aeec6d2202e889327a025f04478423b0f8d7bdc7
UdithSandaruwan <119935572+UdithSandaruwan@users.noreply.github.com>
2023-09-09 09:01:43 +0000

    dynamic data and api rout

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
+Jobs =[
+  {
+    'name' :'sani 01',
+    'age': 5,
+    'salary':'lkr10000'
+  },
+  {
+    'name' :'sani 01',
+    'age': 5,
+  },
+  {
+    'name' :'sani 01',
+    'age': 5,
+    'salary':'lkr10000'
+  }
+]
+
 
 @app.route("/")
-def hello_world():
-  return render_template('home.html')
+def Home():
+  return render_template('home.html',jobs=Jobs)
 
+@app.route("/hire-me")
+def HireMe():
+  return render_template('hire-me.html.html')
+
+@app.route("/api/jobs")
+def listJobs():
+  return jsonify(Jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
